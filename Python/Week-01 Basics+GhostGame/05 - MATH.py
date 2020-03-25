7fcf457c57738439ad38cde4952c0c4d5521989a
VinceThePiDude <44037261+VinceThePiDude@users.noreply.github.com>
2020-03-25 11:44:49 +0000

    Update 05 - MATH.py

--- Python/Week-01 Basics+GhostGame/05 - MATH.py
+++ Python/Week-01 Basics+GhostGame/05 - MATH.py
@@ -9,5 +9,6 @@ N = 10 * 5
 N = (10 - 5) + 1
 
 ##  '=' assignes a variable
-##  '==' checks if it is 'logicly' equal and returnes a boolian(True/False)
+##  '==' checks if it is 'logicly' equal and returns a boolian(True/False)
 ##  '!=' checks if it is NOT 'logicly' equal and returnes a boolian opposide to '=='
+##  BIDMAS rules apply
